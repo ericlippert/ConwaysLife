51236a80fc2cfcef08e3642d5f97aa2519ad9344
Eric Lippert <eric@lippert.com>
2020-04-22 23:46:38 -0700

    Add acorn to patterns.

--- ConwaysLife/Patterns.cs
+++ ConwaysLife/Patterns.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConwaysLife
+﻿namespace ConwaysLife
 {
     static class Patterns
     {
+        // The easiest periodic pattern.
         public static void AddBlinker(this ILife life, LifePoint corner)
         {
             life[corner.X, corner.Y] = true;
             life[corner.X, corner.Y - 1] = true;
             life[corner.X, corner.Y - 2] = true;
+        }
+
+        // This small initial pattern runs for over 5200 ticks before
+        // reaching a stable state of still lifes, blinkers and
+        // gliders.
+        public static void AddAcorn(this ILife life, LifePoint corner)
+        {
+            life[corner.X + 1, corner.Y] = true;
+            life[corner.X + 3, corner.Y - 1] = true;
+            life[corner.X, corner.Y - 2] = true;
+            life[corner.X + 1, corner.Y - 2] = true;
+            life[corner.X + 4, corner.Y - 2] = true;
+            life[corner.X + 5, corner.Y - 2] = true;
+            life[corner.X + 6, corner.Y - 2] = true;
         }
     }
 }
