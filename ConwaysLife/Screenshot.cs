b227bc1f729c06356f15ffa6d98e1e9b1cd5e1a1
Eric Lippert <eric@lippert.com>
2020-04-24 18:28:28 -0700

    Add screenshot helper class.

---
+++ ConwaysLife/Screenshot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace ConwaysLife
+{
+    static class Screenshot
+    {
+        static ImageCodecInfo jpgCodec = null;
+        static EncoderParameters jpgParameters = null;
+
+        static Screenshot()
+        {
+            jpgCodec = ImageCodecInfo.GetImageEncoders().FirstOrDefault(x => x.MimeType == "image/jpeg");
+            if (jpgCodec == null)
+                return;
+            jpgParameters = new EncoderParameters(1);
+            var quality = new EncoderParameter(Encoder.Quality, 75L);
+            jpgParameters.Param[0] = quality;
+        }
+
+        public static void SaveImage(Image image)
+        {
+            var name = Path.GetRandomFileName().Replace(".", "") + ".jpg";
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string path = Path.Combine(desktop, name);
+            image.Save(path, jpgCodec, jpgParameters);
+        }
+    }
+}
