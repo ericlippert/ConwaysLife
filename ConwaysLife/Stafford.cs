8f1efc74d9764f8c392cad84ddd5492d944edf60
Eric Lippert <eric@lippert.com>
2020-05-09 19:02:40 -0700

    Stafford's algorithm with lookup table for transition.

--- ConwaysLife/Stafford.cs
+++ ConwaysLife/Stafford.cs
@@ -114,6 +114,8 @@ namespace ConwaysLife
         public Triplet DecMiddle() => SetMiddleCountRaw(MiddleCountRaw - 1);
         public Triplet IncRight() => SetRightCountRaw(RightCountRaw + 1);
         public Triplet DecRight() => SetRightCountRaw(RightCountRaw - 1);
+
+        public int State => triplet & 0x0fff;
     }
 
     class StaffordUnoptimized : ILife
@@ -514,7 +516,7 @@ namespace ConwaysLife
 
         private int width = 88;
         private Triplet[,] triplets;
-        List<(int, int)> changes;
+        private List<(int, int)> changes;
 
         public StaffordChangeList()
         {
@@ -678,6 +680,274 @@ namespace ConwaysLife
                         t = t.SetMiddleNext(mc == 3 | t.MiddleCurrent & mc == 2);
                         t = t.SetRightNext(rc == 3 | t.RightCurrent & rc == 2);
                         triplets[tx, y] = t;
+                    }
+                }
+            }
+
+            foreach ((int cx, int cy) in previousChanges)
+            {
+                int minx = Max(cx - 1, 1);
+                int maxx = Min(cx + 2, width - 1);
+                int miny = Max(cy - 1, 1);
+                int maxy = Min(cy + 2, height - 1);
+                for (int y = miny; y < maxy; y += 1)
+                {
+                    for (int tx = minx; tx < maxx; tx += 1)
+                    {
+                        bool changed = false;
+
+                        Triplet t = triplets[tx, y];
+                        if (t.LeftCurrent & !t.LeftNext)
+                        {
+                            BecomeDead(tx * 3, y);
+                            changed = true;
+                        }
+                        else if (!t.LeftCurrent & t.LeftNext)
+                        {
+                            BecomeAlive(tx * 3, y);
+                            changed = true;
+                        }
+
+                        if (t.MiddleCurrent & !t.MiddleNext)
+                        {
+                            BecomeDead(tx * 3 + 1, y);
+                            changed = true;
+                        }
+                        else if (!t.MiddleCurrent & t.MiddleNext)
+                        {
+                            BecomeAlive(tx * 3 + 1, y);
+                            changed = true;
+                        }
+
+                        if (t.RightCurrent & !t.RightNext)
+                        {
+                            BecomeDead(tx * 3 + 2, y);
+                            changed = true;
+                        }
+                        else if (!t.RightCurrent & t.RightNext)
+                        {
+                            BecomeAlive(tx * 3 + 2, y);
+                            changed = true;
+                        }
+                        if (changed)
+                            changes.Add((tx, y));
+                    }
+                }
+            }
+        }
+
+        public void Draw(LifeRect rect, Action<LifePoint> setPixel)
+        {
+            long xmin = Max(0, rect.X);
+            long xmax = Min(width * 3, rect.X + rect.Width);
+            long ymin = Max(0, rect.Y - rect.Height + 1);
+            long ymax = Min(height, rect.Y + 1);
+            for (long y = ymin; y < ymax; y += 1)
+                for (long x = xmin; x < xmax; x += 1)
+                    if (this[x, y])
+                        setPixel(new LifePoint(x, y));
+        }
+    }
+
+    class StaffordLookup : ILife
+    {
+        // We're going to keep the top and bottom edge triplets dead,
+        // so this gives us 256 live rows.
+        private int height = 258;
+
+        // This is the width in triplets. That gives us 264 cells, but 
+        // we'll keep the left and right triplets dead, so that's 258
+        // live columns of cells.
+
+        private int width = 88;
+        private Triplet[,] triplets;
+        private List<(int, int)> changes;
+
+        private static Triplet[] lookup;
+        
+        static StaffordLookup()
+        {
+            // Some of these are impossible, but who cares?
+            lookup = new Triplet[1 << 12];
+
+            for (int left = 0; left < 2; left += 1)            
+                for (int middle = 0; middle < 2; middle += 1)
+                    for (int right = 0; right < 2; right +=1)
+                        for (int lc = 0; lc < 8; lc += 1)
+                            for (int mc = 0; mc < 7; mc += 1)
+                                for (int rc = 0; rc < 8; rc += 1)
+                                {
+                                    Triplet t = new Triplet()
+                                        .SetLeftCurrent(left == 1)
+                                        .SetMiddleCurrent(middle == 1)
+                                        .SetRightCurrent(right == 1)
+                                        .SetLeftCountRaw(lc)
+                                        .SetMiddleCountRaw(mc)
+                                        .SetRightCountRaw(rc)
+                                        .SetLeftNext((lc + middle == 3) | (left == 1) & (lc + middle == 2))
+                                        .SetMiddleNext((left + mc + right == 3) | (middle == 1) & (left + mc + right == 2))
+                                        .SetRightNext((middle + rc == 3) | (right == 1) & (middle + rc == 2));
+                                    lookup[t.State] = t;
+                                }
+
+        }
+
+
+        public StaffordLookup()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            triplets = new Triplet[width, height];
+            changes = new List<(int, int)>();
+        }
+
+        private bool IsValidPoint(long x, long y) =>
+            1 <= x && x < (width - 1) * 3 && 1 <= y && y < height - 1;
+
+        private void BecomeAlive(int x, int y)
+        {
+            int tx = x / 3;
+            Triplet t = triplets[tx, y];
+
+            switch (x % 3)
+            {
+                case 0:
+                    if (t.LeftCurrent)
+                        return;
+                    // Left is about to be born
+                    t = t.SetLeftCurrent(true);
+                    triplets[tx, y - 1] = triplets[tx, y - 1].IncMiddle().IncLeft();
+                    triplets[tx, y + 1] = triplets[tx, y + 1].IncMiddle().IncLeft();
+                    triplets[tx - 1, y - 1] = triplets[tx - 1, y - 1].IncRight();
+                    triplets[tx - 1, y] = triplets[tx - 1, y].IncRight();
+                    triplets[tx - 1, y + 1] = triplets[tx - 1, y + 1].IncRight();
+                    break;
+                case 1:
+                    if (t.MiddleCurrent)
+                        return;
+                    // Middle is about to be born
+                    t = t.SetMiddleCurrent(true);
+                    triplets[tx, y - 1] = triplets[tx, y - 1].IncMiddle().IncLeft().IncRight();
+                    triplets[tx, y + 1] = triplets[tx, y + 1].IncMiddle().IncLeft().IncRight();
+                    break;
+                case 2:
+                    if (t.RightCurrent)
+                        return;
+                    // Right is about to be born
+                    t = t.SetRightCurrent(true);
+                    triplets[tx, y - 1] = triplets[tx, y - 1].IncMiddle().IncRight();
+                    triplets[tx, y + 1] = triplets[tx, y + 1].IncMiddle().IncRight();
+                    triplets[tx + 1, y - 1] = triplets[tx + 1, y - 1].IncLeft();
+                    triplets[tx + 1, y] = triplets[tx + 1, y].IncLeft();
+                    triplets[tx + 1, y + 1] = triplets[tx + 1, y + 1].IncLeft();
+                    break;
+            }
+            triplets[tx, y] = t;
+        }
+
+        private void BecomeDead(int x, int y)
+        {
+            int tx = x / 3;
+            Triplet t = triplets[tx, y];
+
+            switch (x % 3)
+            {
+                case 0:
+                    if (!t.LeftCurrent)
+                        return;
+                    t = t.SetLeftCurrent(false);
+                    triplets[tx, y - 1] = triplets[tx, y - 1].DecMiddle().DecLeft();
+                    triplets[tx, y + 1] = triplets[tx, y + 1].DecMiddle().DecLeft();
+                    triplets[tx - 1, y - 1] = triplets[tx - 1, y - 1].DecRight();
+                    triplets[tx - 1, y] = triplets[tx - 1, y].DecRight();
+                    triplets[tx - 1, y + 1] = triplets[tx - 1, y + 1].DecRight();
+                    break;
+                case 1:
+                    if (!t.MiddleCurrent)
+                        return;
+                    t = t.SetMiddleCurrent(false);
+                    triplets[tx, y - 1] = triplets[tx, y - 1].DecMiddle().DecLeft().DecRight();
+                    triplets[tx, y + 1] = triplets[tx, y + 1].DecMiddle().DecLeft().DecRight();
+                    break;
+                case 2:
+                    if (!t.RightCurrent)
+                        return;
+                    t = t.SetRightCurrent(false);
+                    triplets[tx, y - 1] = triplets[tx, y - 1].DecMiddle().DecRight();
+                    triplets[tx, y + 1] = triplets[tx, y + 1].DecMiddle().DecRight();
+                    triplets[tx + 1, y - 1] = triplets[tx + 1, y - 1].DecLeft();
+                    triplets[tx + 1, y] = triplets[tx + 1, y].DecLeft();
+                    triplets[tx + 1, y + 1] = triplets[tx + 1, y + 1].DecLeft();
+                    break;
+            }
+            triplets[tx, y] = t;
+        }
+
+        public bool this[long x, long y]
+        {
+            get
+            {
+                if (IsValidPoint(x, y))
+                {
+                    Triplet t = triplets[x / 3, y];
+                    switch (x % 3)
+                    {
+                        case 0: return t.LeftCurrent;
+                        case 1: return t.MiddleCurrent;
+                        default: return t.RightCurrent;
+                    }
+                }
+                return false;
+            }
+            set
+            {
+                if (IsValidPoint(x, y))
+                {
+                    if (value)
+                    {
+                        if (!this[x, y])
+                        {
+                            BecomeAlive((int)x, (int)y);
+                            changes.Add(((int)x / 3, (int)y));
+                        }
+                    }
+                    else
+                    {
+                        if (this[x, y])
+                        {
+                            BecomeDead((int)x, (int)y);
+                            changes.Add(((int)x / 3, (int)y));
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool this[LifePoint v]
+        {
+            get => this[v.X, v.Y];
+            set => this[v.X, v.Y] = value;
+        }
+
+        public void Step()
+        {
+            var previousChanges = changes;
+            changes = new List<(int, int)>();
+
+            foreach ((int cx, int cy) in previousChanges)
+            {
+                int minx = Max(cx - 1, 1);
+                int maxx = Min(cx + 2, width - 1);
+                int miny = Max(cy - 1, 1);
+                int maxy = Min(cy + 2, height - 1);
+                for (int y = miny; y < maxy; y += 1)
+                {
+                    for (int tx = minx; tx < maxx; tx += 1)
+                    {
+                        triplets[tx, y] = lookup[triplets[tx, y].State];
                     }
                 }
             }
