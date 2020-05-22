0b3c93937983c5dbdd55c2bc43eb45c58193126b
Eric Lippert <eric@lippert.com>
2020-05-22 15:56:06 -0700

    Add support for standard "plaintext" format patterns. Will make it easier to cut-n-paste patterns from the internet.

--- ConwaysLife/Patterns.cs
+++ ConwaysLife/Patterns.cs
@@ -1,69 +1,76 @@
-﻿namespace ConwaysLife
+﻿using System;
+using System.Linq;
+
+namespace ConwaysLife
 {
     static class Patterns
     {
         public const string Acorn = @"
- #
-   #
-##  ###";
+!Name: Acorn
+! 
+.O..... 
+...O... 
+OO..OOO 
+";
 
-        public const string Blinker = "###";
+        public const string Blinker = @"
+!Name: Blinker
+!
+OOO";
 
         public const string Glider = @"
-#
- #
-##";
+!Name: Glider 
+! 
+.O. 
+..O 
+OOO ";
 
         public const string GliderGun = @"
-                        #
-                      # #
-            ##      ##            ##
-           #   #    ##            ##
-##        #     #   ##
-##        #   # ##    # #
-          #     #       #
-           #   #
-            ##";
-        
+!Name: Gosper glider gun 
+! 
+........................O........... 
+......................O.O........... 
+............OO......OO............OO 
+...........O...O....OO............OO 
+OO........O.....O...OO.............. 
+OO........O...O.OO....O.O........... 
+..........O.....O.......O........... 
+...........O...O.................... 
+............OO......................";
 
-        public static void AddPattern(this ILife life, LifePoint corner, string s)
+
+        public static void AddPlaintext(this ILife life, LifePoint corner, string s)
         {
-            long x = corner.X;
+            // The plaintext Life pattern format is described here: https://www.conwaylife.com/wiki/Plaintext
+            var lines = s.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Where(line => !line.StartsWith("!"));
+            
             long y = corner.Y;
-            foreach(char c in s)
+            foreach(var line in lines)
             {
-                switch(c)
+                long x = corner.X;
+                foreach (char c in line)
                 {
-                    case ' ':
-                        x += 1;
-                        break;
-                    case '\r':
-                        break;
-                    case '\n':
-                        x = corner.X;
-                        y += 1;
-                        break;
-                    default:
+                    if (c == 'O')
                         life[x, y] = true;
-                        x += 1;
-                        break;
+                    x += 1;
                 }
+                y += 1;
             }
         }
 
         public static void AddBlinker(this ILife life, LifePoint corner)
         {
-            AddPattern(life, corner, Blinker);
+            AddPlaintext(life, corner, Blinker);
         }
 
         public static void AddAcorn(this ILife life, LifePoint corner)
         {
-            AddPattern(life, corner, Acorn);
+            AddPlaintext(life, corner, Acorn);
         }
 
         public static void AddGliderGun(this ILife life, LifePoint corner)
         {
-            AddPattern(life, corner, GliderGun);
+            AddPlaintext(life, corner, GliderGun);
         }
     }
 }
