aa878b57775851e40a56b230562c21fd85a71ca9
Eric Lippert <eric@lippert.com>
2020-05-25 15:52:30 -0700

    Add a simple memoizer class.

---
+++ ConwaysLife/Memoizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// A simple memoizer. I note that a cache without an expiration policy
+// is a memory leak; we will come back to this point later in the series.
+sealed class Memoizer<A, R>
+{
+    private readonly Dictionary<A, R> dict;
+    public Func<A, R> MemoizedFunc { get; }
+    public Memoizer(Func<A, R> f)
+    {
+        dict = new Dictionary<A, R>();
+        MemoizedFunc = (A a) =>
+        {
+            if (!dict.TryGetValue(a, out R r))
+            {
+                r = f(a);
+                dict.Add(a, r);
+            }
+            return r;
+        };
+    }
+}
