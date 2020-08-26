b5925aa943ba8b2b43c23afde7f7106eeaace365
Eric Lippert <eric@lippert.com>
2020-08-26 10:00:28 -0700

    Add cache manager

---
+++ ConwaysLife/CacheManager.cs
@@ -0,0 +1,10 @@
+﻿namespace ConwaysLife
+{
+    // This class manages the various caches used by Gosper's algorithm.
+    static class CacheManager
+    {
+        public static Memoizer<int, Quad> EmptyMemoizer { get; set; }
+
+        public static Memoizer<(Quad, Quad, Quad, Quad), Quad> MakeQuadMemoizer { get; set; }
+    }
+}
